5bf6afcabb9152ae8348fbd2b7aa559e39e660a5
Bishalkumarshah <143621752+Bishalkumarshah@users.noreply.github.com>
2023-09-01 14:48:43 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+// singleton
+// object.create
+
+// object literals
+
+const JsUser = {
+    name: "bishal",
+    age: 18,
+    location: "Kathmandu",
+    email: "Bishal@341gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser);
+console.log(JsUser.email);
