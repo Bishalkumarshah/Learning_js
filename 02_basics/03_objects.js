2fcd43cd9f129764608a7757804d76f9b67b03e6
Bishalkumarshah <143621752+Bishalkumarshah@users.noreply.github.com>
2023-09-05 12:28:38 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,8 +3,12 @@
 
 // object literals
 
+const mySym = Symbol("key1")
+
 const JsUser = {
     name: "bishal",
+    "full name": "Bishal kumar shah",
+    [mySym]: "mykey1",
     age: 18,
     location: "Kathmandu",
     email: "Bishal@341gmail.com",
@@ -13,4 +17,25 @@ const JsUser = {
 }
 
 // console.log(JsUser);
-console.log(JsUser.email);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+
+JsUser.email = "vishal@123@gmail.com"
+// Object.freeze(JsUser)
+JsUser.email = "shishal@123@gmail.com"
+
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hello Js user");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`hello Js user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
