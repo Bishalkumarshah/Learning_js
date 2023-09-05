9ce74c40ca716db1bf26cb0dc600f03b23385aba
Bishalkumarshah <143621752+Bishalkumarshah@users.noreply.github.com>
2023-09-05 12:49:01 +0000

    objects1

---
+++ 02_basics/04_objects2.js
@@ -0,0 +1,12 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "bishal321"
+tinderUser.name = "sammy"
+tinderUser.isLoogedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    
+}
