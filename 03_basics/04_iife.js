445be386cab4d3ae9bafe7b97c32f516c1fb083d
Bishalkumarshah <143621752+Bishalkumarshah@users.noreply.github.com>
2023-09-10 12:22:14 +0000

    IIFE

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,6 +1,17 @@
 //  Immediately Invoked  Function Expresions (IIFE)
 
 
-function chai(){
-    
-}
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();  // IIFE always close ';' then you run next IIFE
+
+( () => {
+    // Simple IIFE
+    console.log(`DB CONNECTED TWO`);
+})();  // IIFE always close ';' then you run next IIFE
+
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})("Bishal");
