d8c6252d6d6f54c4d314ec90c224d4d00a42f609
Bishalkumarshah <143621752+Bishalkumarshah@users.noreply.github.com>
2023-09-19 06:13:08 +0000

    changes

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,13 +1,15 @@
 const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNumbers.map( (num)=> { return num + 10})
-
 //map always return opertion values like +, *, /, etc
 
+// const newNums = myNumbers.map( (num) => {
+//   return num + 10
+// })
+// console.log(newNums);
+
 const newNums = myNumbers
-                         .map((num) => num * 10) 
-                         .map((num) => num + 1) 
-                         .filter((num) => num >= 40)  
-  //filter always return True or False  
+                  .map((num) => num * 10)
+                  .map((num) => num + 1)
+                  .filter((num) => num >= 40)
 
 console.log(newNums);
--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,31 +1,38 @@
-// const coding = ["js", "ruby", "java", "python", "cpp"]
+const coding = ["js","ruby","python","cpp","java"]
 
-// forEach is not return any values
+// it not store any variables
 
-// const value = coding.forEach( (item)=>{
-//     console.log(item);
-//     return item
+// const values = coding.forEach((items)=>{
+//        console.log(items);
+//       return items
 // })
 
-// console.log(value);
+// console.log(values);
 
+
+// this filter
+ //filter always return True or False or check the condition
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num)=> {
-//     return num > 4
-// } )
-
-// const newNums = []
-
-// myNums.forEach( (num)=>{
-//      if (num > 4) {
-//         newNums.push(num)
-//      }
-// } )
-
-
+// const newNums = myNums.filter( (num) => {
+//    return num > 4
+// })
 // console.log(newNums);
 
+
+// using forEach loop
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//       if (num > 4) {
+//         newNums.push(num)
+//       }
+// })
+// console.log(newNums);
+
+
+
+// Examples of books
 
 const books = [
     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
@@ -38,10 +45,11 @@ const books = [
     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
   ];
+let userBooks = books.filter( (bk)=> bk.genre === 'History')
 
-  let userBooks = books.filter((bk)=> bk.genre === "History")
-
-  userBooks = books.filter( (bk) => {
-     return bk.publish >= 1995 && bk.genre === "History"
-  })
+userBooks = books.filter( (bk) => {
+  return  bk.publish >= 1995  && bk.genre === 'History'
+})
   console.log(userBooks);
+
+ 
